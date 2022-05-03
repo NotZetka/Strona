1bf0406e186dda7a9b9b88483208019db248a69d
Zetka <jakub.kochanski10@gmail.com>
2022-05-03 12:48:54 +0200

    added projects page

---
+++ Jakub Kochański/Pages/Projekty.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Jakub_Kochański.Pages
+{
+    public class ProjektyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
